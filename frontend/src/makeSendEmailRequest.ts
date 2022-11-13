a941e9ca44e61e71ad1161a06a3da596624c1ccd
Steffen Holanger <44159536+spiftire@users.noreply.github.com>
2022-11-13 19:57:48 +0100

    add status state

--- frontend/src/makeSendEmailRequest.ts
+++ frontend/src/makeSendEmailRequest.ts
@@ -1,12 +1,24 @@
 const API_URL = 'https://toggl-hire-frontend-homework.onrender.com'
 export const makeSendEmailsRequest = async (emails: ReadonlyArray<string>) => {
-  return (
-    await fetch(`${API_URL}/api/send`, {
+  try {
+    const response = await fetch(`${API_URL}/api/send`, {
       headers: {
         'Content-Type': 'application/json',
       },
       method: 'POST',
       body: JSON.stringify({ emails }),
     })
-  ).json()
+    if (response.status !== 200) {
+      throw response
+    }
+    const parsedResponse = await response.json()
+    return parsedResponse
+  } catch (err) {
+    if (err instanceof Error) {
+      console.log('🚀 ~ makeSendEmailsRequest ~ err', { err })
+    }
+    throw err
+  }
 }
+
+type APIError = { emails: string[]; error: 'send_failure' }
--- frontend/src/useEmailSender.tsx
+++ frontend/src/useEmailSender.tsx
@@ -5,6 +5,7 @@ export const useEmailSender = () => {
   const [state, dispatch] = useReducer<typeof emailSenderReducer>(emailSenderReducer, {
     isLoading: false,
     error: undefined,
+    status: 'IDLE',
   })
 
   const sendEmails = useCallback(async (emails: ReadonlyArray<string>) => {
@@ -13,23 +14,37 @@ export const useEmailSender = () => {
       await makeSendEmailsRequest(emails)
       dispatch({ type: 'success' })
     } catch (error) {
+      console.error('🚀 ~ sendEmails ~ error', error)
+      if (error instanceof Error && error.message.includes('Unexpected end of JSON')) {
+        return dispatch({ type: 'success' })
+      }
       dispatch({ type: 'error', error })
     }
   }, [])
-  return { ...state, sendEmails }
+
+  const reset = useCallback(() => {
+    dispatch({ type: 'reset' })
+  }, [])
+
+  return { ...state, sendEmails, reset }
 }
 type ReducerActions =
   | { type: 'success' }
   | { type: 'send emails'; emails: ReadonlyArray<string> }
   | { type: 'error'; error: unknown }
-type State = { isLoading: boolean; error?: unknown }
+  | { type: 'reset' }
+export type Status = 'SUCCESS' | 'FAILED' | 'IDLE' | 'SENDING'
+
+type State = { isLoading: boolean; error?: unknown; status: Status }
 const emailSenderReducer = (state: State, action: ReducerActions): State => {
   switch (action.type) {
     case 'success':
-      return { ...state, isLoading: false }
+      return { ...state, isLoading: false, status: 'SUCCESS' }
     case 'send emails':
-      return { ...state, isLoading: true, error: undefined }
+      return { ...state, isLoading: true, error: undefined, status: 'SENDING' }
     case 'error':
-      return { ...state, isLoading: false, error: action.error }
+      return { ...state, isLoading: false, error: action.error, status: 'FAILED' }
+    case 'reset':
+      return { ...state, isLoading: false, error: undefined, status: 'IDLE' }
   }
 }
