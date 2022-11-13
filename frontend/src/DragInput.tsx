dab0e246876245fcd13b06250ab80c02c919fb5f
Steffen Holanger <44159536+spiftire@users.noreply.github.com>
2022-11-13 19:23:27 +0100

    add logic to show file list

--- frontend/src/DragInput.tsx
+++ frontend/src/DragInput.tsx
@@ -1,45 +1,59 @@
-import { ChangeEventHandler, DragEventHandler, FC, useEffect, useRef, useState } from 'react'
+import { ChangeEventHandler, DragEventHandler, FC, useRef, useState } from 'react'
 import './DragInput.css'
+import { Spinner } from './Spinner'
+import { useEmailSender } from './useEmailSender'
 
 /**
  * Inspired by https://www.codemzy.com/blog/react-drag-drop-file-upload
  */
-export const DragInput: FC<{ onEmailChange: (emails: ReadonlyArray<string>) => void }> = ({ onEmailChange }) => {
-  const { handleDrag, dragActive, handleDrop, handleChange, emails } = useDragInput()
-
-  useEffect(() => {
-    if (emails !== undefined) {
-      onEmailChange(emails)
-    }
-  }, [emails, onEmailChange])
+export const DragInput: FC = () => {
+  const { handleDrag, dragActive, handleDrop, handleChange, fileNames, handleSendEmail, isLoading } = useDragInput()
 
   const inputRef = useRef<HTMLInputElement>(null)
   return (
-    <form id="form-file-upload" onDragEnter={handleDrag}>
-      <input type="file" id="input-file-upload" multiple={true} onChange={handleChange} ref={inputRef} />
-      <label id="label-file-upload" htmlFor="input-file-upload" className={dragActive ? 'drag-active' : ''}>
-        <div>
-          <p>Drag and drop your file here or</p>
-          <button className="upload-button" onClick={() => inputRef.current?.click()}>
-            Upload a file
+    <>
+      <form id="form-file-upload" onDragEnter={handleDrag}>
+        <input type="file" id="input-file-upload" multiple={true} onChange={handleChange} ref={inputRef} />
+        <label id="label-file-upload" htmlFor="input-file-upload" className={dragActive ? 'drag-active' : ''}>
+          <div>
+            <p>Drag and drop your file here or</p>
+            <button className="upload-button" onClick={() => inputRef.current?.click()}>
+              Upload a file
+            </button>
+          </div>
+          {dragActive && (
+            <div
+              id="drag-file-element"
+              onDragEnter={handleDrag}
+              onDragLeave={handleDrag}
+              onDragOver={handleDrag}
+              onDrop={handleDrop}
+            />
+          )}
+        </label>
+      </form>
+      {fileNames && (
+        <>
+          <ul>
+            {fileNames.map((fn) => (
+              <li key={fn}>{fn}</li>
+            ))}
+          </ul>
+          <button onClick={handleSendEmail} disabled={isLoading}>
+            Send emails {isLoading && <Spinner />}
           </button>
-        </div>
-        {dragActive && (
-          <div
-            id="drag-file-element"
-            onDragEnter={handleDrag}
-            onDragLeave={handleDrag}
-            onDragOver={handleDrag}
-            onDrop={handleDrop}
-          />
-        )}
-      </label>
-    </form>
+        </>
+      )}
+    </>
   )
 }
 const useDragInput = () => {
+  const { sendEmail: sendEmails, ...state } = useEmailSender()
   const [dragActive, setDragActive] = useState(false)
   const [emails, setEmails] = useState<ReadonlyArray<string>>()
+  const [fileList, setFileList] = useState<FileList>()
+
+  const fileNames = fileList && Array.from(fileList).map((f) => f.name)
 
   // handle drag events
   const handleDrag: DragEventHandler<HTMLFormElement | HTMLDivElement> = function (e) {
@@ -57,6 +71,7 @@ const useDragInput = () => {
     e.stopPropagation()
     setDragActive(false)
     if (e.dataTransfer.files && e.dataTransfer.files[0]) {
+      setFileList(e.dataTransfer.files)
       extractEmails(e.dataTransfer.files)
     }
   }
@@ -65,7 +80,18 @@ const useDragInput = () => {
   const handleChange: ChangeEventHandler<HTMLInputElement> = function (e) {
     e.preventDefault()
     if (e.target.files && e.target.files[0]) {
+      setFileList(e.target.files)
       extractEmails(e.target.files)
+    }
+  }
+
+  const handleSendEmail = () => {
+    if (emails !== undefined) {
+      try {
+        sendEmails(emails)
+      } catch (err) {
+        console.log(err)
+      }
     }
   }
 
@@ -95,7 +121,7 @@ const useDragInput = () => {
     }
   }
 
-  return { handleDrag, dragActive, handleDrop, handleChange, emails }
+  return { handleDrag, dragActive, handleDrop, handleChange, handleSendEmail, emails, fileNames, ...state }
 }
 
 const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/
